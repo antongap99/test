79257a1c2381438be46f26c5302c1094cb11a5a0
Anton <anton.gap99@gmail.com>
2023-01-27 18:30:08 +0300

    feat: add new slider

--- script/api/slider.js
+++ script/api/slider.js
@@ -1,60 +1,175 @@
 export const sliderController = () => {
     const cards = document.querySelectorAll('.sale__card');
     const cardsWrapper = document.querySelector('.sale__cards');
-    cardsWrapper.style.overflow = 'hidden';
     const slider = document.querySelector('.sale__slider');
     const buttonPrev = document.querySelector('.sale__button-prev')
     const buttonNext = document.querySelector('.sale__button-next')
     let count = 0;
-    let initialWidth = 1300;
+    const html = document.documentElement;
 
-
-    const resize = () => {
-        cardsWrapper.style.width = `${initialWidth++}px`
-        console.log(document.documentElement.clientWidth);
-        switch (document.documentElement.clientWidth) {
-            case 940:
-                initialWidth = 1100
-                break;
-            case 610:
-                initialWidth = 1100
-                break;
-            case 380:
-                initialWidth = 1000
-                break;
-            case 320:
-                initialWidth = 980
-                break;
-            default:
-                break;
+    const CardHedden = (cards, num) => {
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove('sale__card_hidden')
         }
-
+        for (let i = num; i < cards.length; i++) {
+            cards[i].classList.add('sale__card_hidden')
+        }
     }
 
-    buttonNext.addEventListener('click', () => {
+    const resize = () => {
+        if (html.clientWidth > 1106) {
+            CardHedden(cards, 4)
+        }
+        if (html.clientWidth <= 1106 && html.clientWidth > 830) {
+            CardHedden(cards, 3)
+        }
+        if (html.clientWidth <= 830 && html.clientWidth > 560) {
+            CardHedden(cards, 2)
+        }
+        if (html.clientWidth <= 560) {
+            CardHedden(cards, 1)
+        }
+    }
 
-        if (slider.offsetWidth !== cardsWrapper.offsetWidth) {
-            count++
-            if (count > cards.length - 1) {
-                count = cards.length - 1;
+
+
+buttonNext.addEventListener('click', () => {
+    if (document.documentElement.clientWidth <= 560) {
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (cards[i + 1] && cards[i + 1].classList.contains('sale__card_hidden')) {
+                    cards[i].classList.add('sale__card_hidden');
+                    cards[i + 1].classList.remove('sale__card_hidden');
+                    break
+                }
             }
-            let width = `${cards[0].offsetWidth + 30}`
-            cardsWrapper.style.transform = `translate(${-(count * width)}px)`
         }
+    }
+    if (document.documentElement.clientWidth > 560 && document.documentElement.clientWidth <= 830) {
+        let outFromQ = false
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (!outFromQ && cards[i + 2]) {
+                    cards[i].classList.add('sale__card_hidden');
+                    outFromQ = true;
+                }
 
-    })
-    buttonPrev.addEventListener('click', () => {
-        count--
-        if (count < 0) {
-            count = 0;
+                if (cards[i + 1] && cards[i + 1].classList.contains('sale__card_hidden')) {
+                    cards[i + 1].classList.remove('sale__card_hidden');
+                    break
+                }
+            }
         }
-        let width = `${cards[0].offsetWidth + 40}`
-        cardsWrapper.style.transform = `translate(${-(count * width)}px)`
-    })
+    }
+    if (document.documentElement.clientWidth > 830 && document.documentElement.clientWidth <= 1106) {
+        let outFromQ = false
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (!outFromQ && cards[i + 3]) {
+                    cards[i].classList.add('sale__card_hidden');
+                    outFromQ = true;
+                }
 
-    window.addEventListener('resize', () => {
-        requestAnimationFrame(resize)
-    })
-    resize();
+                if (cards[i + 1] && cards[i + 1].classList.contains('sale__card_hidden')) {
+                    cards[i + 1].classList.remove('sale__card_hidden');
+                    break
+                }
+
+            }
+        }
+    }
+    if (document.documentElement.clientWidth > 1106) {
+        let outFromQ = false
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (!outFromQ && cards[i + 4]) {
+                    cards[i].classList.add('sale__card_hidden');
+                    outFromQ = true;
+                }
+
+                if (cards[i + 1] && cards[i + 1].classList.contains('sale__card_hidden')) {
+                    cards[i + 1].classList.remove('sale__card_hidden');
+                    break
+                }
+
+            }
+        }
+    }
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (document.documentElement.clientWidth <= 560) {
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (cards[i - 1] && cards[i - 1].classList.contains('sale__card_hidden')) {
+                    cards[i].classList.add('sale__card_hidden');
+                    cards[i - 1].classList.remove('sale__card_hidden');
+                    break
+                }
+            }
+        }
+    }
+    if (document.documentElement.clientWidth > 560 && document.documentElement.clientWidth <= 830) {
+        let outFromQ = false
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (cards[i - 1]) {
+                    if (cards[i - 1].classList.contains('sale__card_hidden')) {
+                        cards[i - 1].classList.remove('sale__card_hidden');
+                        continue;
+                    }
+
+                    if (!cards[i + 1] || cards[i + 1].classList.contains('sale__card_hidden')) {
+                        cards[i].classList.add('sale__card_hidden');
+                        break
+                    }
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    if (document.documentElement.clientWidth > 830 && document.documentElement.clientWidth <= 1106) {
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (cards[i - 1]) {
+                    if (cards[i - 1].classList.contains('sale__card_hidden')) {
+                        cards[i - 1].classList.remove('sale__card_hidden');
+                        continue;
+                    }
+
+                    if (!cards[i + 1] || cards[i + 1].classList.contains('sale__card_hidden')) {
+                        cards[i].classList.add('sale__card_hidden');
+                        break
+                    }
+                } else {
+                    break
+                }
+
+            }
+        }
+    }
+    if (document.documentElement.clientWidth > 1106) {
+        for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains('sale__card_hidden')) {
+                if (cards[i - 1]) {
+                    if (cards[i - 1].classList.contains('sale__card_hidden')) {
+                        cards[i].classList.remove('sale__card_hidden');
+                    }
+                    if (cards[i + 1] && cards[i + 1].classList.contains('sale__card_hidden')) {
+                        cards[i].classList.add('sale__card_hidden');
+                        break
+                    }
+                }
+            }
+        }
+    }
+})
+
+
+window.addEventListener('resize', () => {
+    resize()
+})
+resize();
 
 }
