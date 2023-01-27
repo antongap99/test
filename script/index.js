54905b5ad123de46d4138b163e47f9a0868620aa
Anton <anton.gap99@gmail.com>
2023-01-27 18:48:45 +0300

    ref: some changes

--- script/index.js
+++ script/index.js
@@ -1,10 +1,8 @@
 import { selectControl } from './modules/selectControl.js'
 import { saleControl } from './modules/saleControl.js'
-import swiperController from './api/swiperController.js';
 import {sliderController} from './api/slider.js';
 
 const init = () => {
-    // swiperController();
     sliderController();
     selectControl();
     saleControl();
